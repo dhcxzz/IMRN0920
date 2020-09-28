d36aa1453c9c3dd75607578aa35b260478dad116
syarifhilmi <dhonisy@gmail.com>
2020-09-29 05:15:12 +0700

    update tugas 4

---
+++ Tugas 4 - Function/functions.js
@@ -0,0 +1,27 @@
+//Soal 1
+function teriak(){
+    return '"Halo Sanber!"';
+}
+console.log(teriak());
+
+//Soal 2
+function kalikan(num1,num2){
+    return num1*num2;
+}
+var num1 = 12
+var num2 = 4
+ 
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali) //48
+
+//Soal 3
+function introduce(name,age,address,hobby){
+    return '"Nama saya '+name+', umur saya '+age+' tahun, alamat saya di '+address+', dan saya punya hobby yaitu '+hobby+'!"';
+}
+var name = "Agus"
+var age = 30
+var address = "Jln. Malioboro, Yogyakarta"
+var hobby = "Gaming"
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan) // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogyakarta, dan saya punya hobby yaitu Gaming!" 
