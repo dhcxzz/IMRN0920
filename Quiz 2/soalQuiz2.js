6ae5b6a2ebc86c9950edf6e0653f020bf59aa4cd
syarifhilmi <dhonisy@gmail.com>
2020-10-10 22:33:53 +0700

    update quiz

---
+++ Quiz 2/soalQuiz2.js
@@ -0,0 +1,140 @@
+/**
+ * Berikut soal quiz kali ini, terdiri dari 3 Soal
+ * Kerjakan dengan sebaik mungkin, dengan menggunakan metode yang telah dipelajari,
+ * Tidak diperkenankan untuk menjawab hanya dengan console.log('teks jawaban');
+ * maupun dengan terlebih dahulu memasukkannya ke dalam variabel, misal var a = 'teks jawaban'; console.log(a);
+ * 
+ * Terdapat tambahan poin pada setiap soal yang dikerjakan menggunakan sintaks ES6 (+5 poin)
+ * Jika total nilai Anda melebihi 100 (nilai pilihan ganda + coding), tetap akan memiliki nilai akhir yaitu 100
+ * 
+ * Selamat mengerjakan
+*/
+
+/*========================================== 
+  1. SOAL CLASS SCORE (10 poin + 5 Poin ES6)
+  ==========================================
+  Buatlah sebuah class dengan nama Score. class Score tersebut memiliki properti "subject", "points", dan "email". 
+  "points" dapat di input berupa number (1 nilai) atau array of number (banyak nilai).
+  tambahkan method average untuk menghitung rata-rata dari parameter points ketika yang di input berupa array (lebih dari 1 nilai)
+*/
+
+class Score {
+  constructor(subject,points,email){
+    this.subject=subject
+    this.points=points
+    this.email=email
+  }
+ 
+  display(){
+    return `{email: "${this.email}", subject: "${this.subject}", points: ${this.points}}`
+  }
+  
+  /*average(){
+    let sum = 0;
+    for( let i = 0; i < this.points.length; i++ ){
+        sum += parseInt( this.points[i])
+    }
+
+    var avg = sum/this.points.length
+    }*/
+}
+
+/*=========================================== 
+  2. SOAL Create Score (10 Poin + 5 Poin ES6)
+  ===========================================
+  Membuat function viewScores yang menerima parameter data berupa array multidimensi dan subject berupa string
+  Function viewScores mengolah data email dan nilai skor pada parameter array 
+  lalu mengembalikan data array yang berisi object yang dibuat dari class Score.
+  Contoh: 
+
+  Input
+   
+  data : 
+  [
+    ["email", "quiz-1", "quiz-2", "quiz-3"],
+    ["abduh@mail.com", 78, 89, 90],
+    ["khairun@mail.com", 95, 85, 88]
+  ]
+  subject: "quiz-1"
+
+  Output 
+  [
+    {email: "abduh@mail.com", subject: "quiz-1", points: 78},
+    {email: "khairun@mail.com", subject: "quiz-1", points: 95},
+  ]
+*/
+
+const data = [
+  ["email", "quiz - 1", "quiz - 2", "quiz - 3"],
+  ["abduh@mail.com", 78, 89, 90],
+  ["khairun@mail.com", 95, 85, 88],
+  ["bondra@mail.com", 70, 75, 78],
+  ["regi@mail.com", 91, 89, 93]
+]
+
+viewScores=(data, subject)=> {
+   
+  if(subject=="quiz-1"){
+    
+    let siswa1 = new Score (subject, data[1][1],data[1][0])
+    let siswa2 = new Score (subject, data[2][1],data[2][0])
+    let siswa3 = new Score (subject, data[3][1],data[3][0])
+    let temp=[siswa1.display(),siswa2.display(),siswa3.display()]
+    console.log(temp)
+  }
+  else if (subject=="quiz-2"){
+    let siswa1 = new Score (subject, data[1][2],data[1][0])
+    let siswa2 = new Score (subject, data[2][2],data[2][0])
+    let siswa3 = new Score (subject, data[3][2],data[3][0])
+    let temp=[siswa1.display(),siswa2.display(),siswa3.display()]
+    console.log(temp)
+  }
+  else if (subject=="quiz-3"){
+    let siswa1 = new Score (subject, data[1][3],data[1][0])
+    let siswa2 = new Score (subject, data[2][3],data[2][0])
+    let siswa3 = new Score (subject, data[3][3],data[3][0])
+    let temp=[siswa1.display(),siswa2.display(),siswa3.display()]
+    console.log(temp)
+  }
+  
+}
+
+// TEST CASE
+viewScores(data, "quiz-1")
+viewScores(data, "quiz-2")
+viewScores(data, "quiz-3")
+
+/*==========================================
+  3. SOAL Recap Score (15 Poin + 5 Poin ES6)
+  ==========================================
+    Buatlah fungsi recapScore yang menampilkan perolehan nilai semua student. 
+    Data yang ditampilkan adalah email user, nilai rata-rata, dan predikat kelulusan. 
+    predikat kelulusan ditentukan dari aturan berikut:
+    nilai > 70 "participant"
+    nilai > 80 "graduate"
+    nilai > 90 "honour"
+
+    output:
+    1. Email: abduh@mail.com
+    Rata-rata: 85.7
+    Predikat: graduate
+
+    2. Email: khairun@mail.com
+    Rata-rata: 89.3
+    Predikat: graduate
+
+    3. Email: bondra@mail.com
+    Rata-rata: 74.3
+    Predikat: participant
+
+    4. Email: regi@mail.com
+    Rata-rata: 91
+    Predikat: honour
+
+*/
+
+function recapScores(data) {
+  // code kamu di sini
+}
+
+recapScores(data);
