cf1cb09d229a4b1b029dd11c160f370038c6edf7
syarifhilmi <dhonisy@gmail.com>
2020-10-18 23:34:02 +0700

    update bottom tab

--- SanberApp/Tugas/Quiz3/index.js
+++ SanberApp/Tugas/Quiz3/index.js
@@ -13,6 +13,9 @@ import Login from './Login';
 import Register from './Register';
 import Home from './HomeScreen';
 import Splash from './Splash';
+import Cart from './cart';
+import Message from './message';
+import Profile from './profile';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -24,12 +27,22 @@ const index = () => {
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={TabBawah} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const TabBawah = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="Message" component={Message} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
 export default index;
 
 const styles = StyleSheet.create({});
